b09eb4506aefd1e59719c3853c2c26e865c425da
Huang Xirui <legend1949@vip.qq.com>
2019-07-16 13:46:50 +0800

    增加用户访问过滤器，对非法访问进行拦截

---
+++ src/filters/Authenticate.java
@@ -0,0 +1,44 @@
+package filters;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import entities.UserInfo;
+
+/**
+ * @author Dragon1573
+ * @date 2019/7/16
+ */
+@WebFilter(filterName = "Authenticate", urlPatterns = {"/private/*", "/servlets/private/*"})
+public class Authenticate implements javax.servlet.Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        // 获取所需对象
+        HttpServletRequest request = (HttpServletRequest)req;
+        HttpServletResponse response = (HttpServletResponse)resp;
+        HttpSession session = request.getSession();
+
+        // 设置编码格式
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        // 进行过滤
+        UserInfo info = (UserInfo)session.getAttribute("certificate");
+        if (info == null || "Anonymous".equals(info.getUsername()) || !info.isVerified()) {
+            PrintWriter out = response.getWriter();
+            out.println("<h1 style='text-align: center'>非法访问！正在返回主页</h1>");
+
+            // 延时跳转
+            response.setHeader("refresh", "3; url=../index.jsp");
+        } else {
+            chain.doFilter(req, resp);
+        }
+    }
+}
